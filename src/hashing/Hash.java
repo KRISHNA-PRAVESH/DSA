925a9c604721a0221554f9021c2d05f88587dba2
KRISHNA-PRAVESH <krishnapravesh.hj@gmail.com>
2023-04-08 18:09:31 +0530

    Hashing

---
+++ src/hashing/Hash.java
@@ -0,0 +1,12 @@
+package hashing;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hash {
+    public static void main(String args[]){
+      HashMap<Integer,Integer> map = new HashMap<>();
+
+
+    }
+
+}
